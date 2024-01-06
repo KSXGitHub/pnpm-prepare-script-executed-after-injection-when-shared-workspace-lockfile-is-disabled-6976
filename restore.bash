1698bfd06559997807dc7976e788c621cc80d077
khai96_ <hvksmr1996@gmail.com>
2024-01-06 15:45:35 +0700

    add -v

--- restore.bash
+++ restore.bash
@@ -3,4 +3,4 @@ set -o errexit -o pipefail -o nounset
 cd "$(dirname "$0")"
 
 echo 'Cleaning up...' >&2
-pretty-exec -- rm -rf {.,packages/{a,b}}/{node_modules,pnpm-lock.yaml} packages/b/test.txt
+pretty-exec -- rm -rfv {.,packages/{a,b}}/{node_modules,pnpm-lock.yaml} packages/b/test.txt
