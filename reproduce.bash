b233857a03c7e56382f3a75d7d26ee6a897d9a81
khai96_ <hvksmr1996@gmail.com>
2024-01-06 15:43:09 +0700

    set --reporter=append-only

--- reproduce.bash
+++ reproduce.bash
@@ -3,6 +3,6 @@ set -o errexit -o pipefail -o nounset
 cd "$(dirname "$0")"
 
 echo 'Reproducing...' >&2
-pretty-exec -- run-at packages/a pd install
+pretty-exec -- run-at packages/a pd install --reporter=append-only
 pretty-exec -- ls -A packages/a/node_modules/@test/b
 pretty-exec -- bat packages/a/node_modules/@test/b/test.txt
